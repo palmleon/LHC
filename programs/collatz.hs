482276e97d496343029ac7a945413591ae33f2c1
palmleon <leonardo.palmucci@gmail.com>
2021-12-16 16:46:59 +0100

    chore: update sample programs

--- programs/collatz.hs
+++ programs/collatz.hs
@@ -1,10 +1,12 @@
-collatz :: Int -> Int
+collatz :: Int -> Int -> Int
 isEven :: Int -> Bool
-x :: Int
-x = 25
+x' :: Int; x' = 25
 isEven n = (mod n 2 == 0)
-collatz n = if (n < 1) then -1 else
-            if (n == 1) then 1 else
-			if (isEven n) then (collatz (n div 2)) else (collatz ((3*n + 1)))
+collatz n i = if (n < 1) then -1 else
+               if (n == 1) then i else
+			   if (isEven n) then (collatz (n div 2) (i+1)) else (collatz (3*n + 1) (i+1))
 
-main = print (collatz n)
+main = do
+       print "Applying the Collatz Procedure to 25 needs"
+       print (collatz x' 0)
+       print "steps"
--- programs/list_min.hs
+++ programs/list_min.hs
@@ -1,12 +1,16 @@
-list_min :: [Double] -> Double -> Int -> Int
+list_min :: [Double] -> Double -> Int -> Double
 list_min xs min i = if (i >= elem xs)
                         then min
-                        else
+                        else 
                             let x :: Double; x = xs !! 1
                             in							
-							  if x < min
-							    then list_min xs x (i+1)
-                                else list_min xs min (i+1)
-list :: [Double]
-list = [5.0, 2.0, 19.0, 34.0, 1.0]
-main = print (list_min list 100.0 0)
+							 if x < min
+							  then list_min xs x (i+1)
+                              else list_min xs min (i+1)
+main = do
+       let list :: [Double]; x :: Double
+           x = -19.5e0
+           list = [5.0, 2.0, x, 34.0, 1.0]  
+       print (list_min list 100.0 0)
+   -- result should be -19.5
+   
